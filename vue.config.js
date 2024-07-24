91fb44534ab1629fd448a1b0f165bb21bd57087b
jhuix@sohu.com <jhuix@sohu.com>
2024-07-24 10:26:38 +0800

    Update showdowns deps to version 0.5.7.

--- vue.config.js
+++ vue.config.js
@@ -63,7 +63,7 @@ module.exports = {
       // 在 dist/index.html 的输出
       filename: 'index.html',
       // 当使用 title 选项时，template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'markdown-showdowns-editor',
+      title: 'vue-showdowns-editor',
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
       chunks: ['chunk-vendors', 'chunk-common', 'index']
